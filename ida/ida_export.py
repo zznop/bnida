1f405d9344b1d7c730237b23d265a3dcea0772c2
Brandon Miller <brandonkmiller@protonmail.com>
2019-12-21 00:26:43 -0500

    Updated IDA scripts to python 3
    Fixed deprecated API calls, cleaned up code, and made it so exported
    JSON files from ida_export.py are compatible with ida_import.py. I would
    like a single common format for IDA, BN, and in the future, Ghidra. I
    also improved comment exporting and hopefully implemented a fix that
    will improve structure imports in BN.

--- ida/ida_export.py
+++ ida/ida_export.py
@@ -1,48 +1,118 @@
-"""ida_export.py: exports IDA analysis data to a json file"""
-
-__author__      = "zznop"
-__copyright__   = "Copyright 2018, zznop0x90@gmail.com"
-__license__     = "WTFPL"
-
-import idc
+import ida_struct
+import ida_kernwin
+import ida_segment
+import ida_bytes
 import idautils
 import json
 
-def get_single_comment(ea, is_func=False):
-    """IDA has repeatable and regular comments. BN only has regular comments.
+"""
+Exports analysis data from IDA to a bnida json file
+"""
+
+__author__    = 'zznop'
+__copyright__ = 'Copyright 2018, zznop0x90@gmail.com'
+__license__   = 'WTFPL'
+
+def get_single_comment(regular, repeatable):
+    """
+    IDA has repeatable and regular comments. BN only has regular comments.
     This function constructs a single comment from both repeatable and regular
     comments
     """
-    regular_comment    = ""
-    repeatable_comment = ""
 
-    if is_func:
-        regular_comment = idc.GetFunctionCmt(ea, 0)
-        repeatable_comment = idc.GetFunctionCmt(ea, 1)
-    else:
-        regular_comment = idc.Comment(ea)
-        repeatable_comment = idc.RptCmt(ea)
-
-    if regular_comment is None:
-        return repeatable_comment
-    elif repeatable_comment is None:
-        return regular_comment
-    elif repeatable_comment is None and regular_comment is None:
+    if repeatable is None and regular is None:
         return None
-    else:
-        if len(regular_comment) == 0:
-            return repeatable_comment
+    elif repeatable is not None and regular is not None:
+        return regular + '\n' + repeatable
+    elif repeatable is not None and regular is None:
+        return repeatable
+    elif regular is not None and repeatable is None:
+        return regular
 
-        if len(repeatable_comment) == 0:
-            return repeatable_comment
-
-        return regular_comment + "\n" + repeatable_comment
-
-    return None
-
-def get_symbols():
-    """Get symbols from IDA database
+def get_single_function_comment(ea):
     """
+    Get function comment
+
+    :param ea: Function offset
+    :return: comment string or None
+    """
+
+    func = ida_funcs.get_func(ea)
+    regular = ida_funcs.get_func_cmt(func, False)
+    repeatable = ida_funcs.get_func_cmt(func, True)
+    return get_single_comment(regular, repeatable)
+
+def get_single_line_comment(ea):
+    """
+    Get line comment
+
+    :param ea: Function offset
+    :return: comment string or None
+    """
+
+    regular = ida_bytes.get_cmt(ea, False)
+    repeatable = ida_bytes.get_cmt(ea, True)
+    cmt = get_single_comment(regular, repeatable)
+    return cmt
+
+def get_function_comments():
+    """
+    Get function comments from IDA database
+
+    :return: A dictionary of function comments
+    """
+
+    comments = {}
+    for ea in idautils.Functions():
+        comment = get_single_function_comment(ea)
+        if comment:
+            comments[ea] = comment
+
+    return comments
+
+def get_functions():
+    """
+    Get function start addresses
+
+    :return: An array of function addresses
+    """
+
+    func_addrs = []
+    for ea in idautils.Functions():
+        func_addrs.append(ea)
+
+    return func_addrs
+
+def get_line_comments():
+    """
+    Iterate through every address in a segment and check for comments
+
+    :return: Dictionary of comments
+    """
+
+    last_comment = ''
+    comments = {}
+    for ea in idautils.Segments():
+        segm = ida_segment.getseg(ea)
+        name = ida_segment.get_segm_name(segm)
+        if name == 'LOAD':
+            continue
+
+        for i in range(segm.start_ea, segm.end_ea):
+            comment = get_single_line_comment(i)
+            if comment and comment != last_comment:
+                comments[i] = comment
+                last_comment = comment
+
+    return comments
+
+
+def get_names():
+    """
+    Get symbols from IDA database
+    :return: Dictionary of symbols
+    """
+
     symbols = {}
     for addr, name in idautils.Names():
         symbols[addr] = name
@@ -50,100 +120,121 @@ def get_symbols():
     return symbols
 
 def get_sections():
-    """Get section names and start/end addrs from IDA database
     """
+    Get section names and start/end addrs from IDA database
+
+    :return: Dictionary containing segment information
+    """
+
     sections = {}
     for ea in idautils.Segments():
+        segm = ida_segment.getseg(ea)
+        name = ida_segment.get_segm_name(segm)
+        if name == 'LOAD':
+            continue
+
         curr = {}
-        curr["start"] = idc.SegStart(ea)
-        curr["end"]   = idc.SegEnd(ea)
-        sections[idc.SegName(ea)] = curr
+        curr['start']  = segm.start_ea
+        curr['end']    = segm.end_ea
+        sections[name] = curr
 
     return sections
 
-def get_comments():
-    """Get function and instruction comments from IDA database
-    """
-    comments = {}
-    for ea in idautils.Functions():
-        current_function = {}
-        end = idc.GetFunctionAttr(ea, idc.FUNCATTR_END)
-        current_function["comment"] = get_single_comment(ea, True)
-        current_function["comments"] = {}
-        for line_ea in idautils.Heads(ea, end):
-            instr_comment = get_single_comment(line_ea)
-            if instr_comment is not None:
-                current_function["comments"][line_ea] = instr_comment
-        comments[ea] = current_function
-
-    return comments
-
 def get_member_type(struct, idx):
-    """Retrieve the type information for the struct member
     """
+    Retrieve the type information for the struct member
+
+    :return: Type string
+    """
+
     member = ida_struct.get_member(struct, idx)
-    tif =  idaapi.tinfo_t()
-    ida_struct.get_member_tinfo(member, tif)
-    elements = str(tif).split(" ")
-    typ = "unknown"
-    if len(elements) == 2 and elements[0] == "unsigned":
-        if elements[1] == "__int8":
-            typ = "uint8_t"
-        elif elements[1] == "__int16":
-            typ = "uint16_t"
-        elif elements[1] == "__int32":
-            typ = "uint32_t"
-        elif elements[1] == "__int64":
-            typ = "uint64_t"
+    tif = idaapi.tinfo_t()
+    ida_struct.get_member_tinfo(tif, member)
+    elements = str(tif).split(' ')
+    typ = None
+    if len(elements) == 2 and elements[0] == 'unsigned':
+        if elements[1] == '__int8':
+            typ = 'uint8_t'
+        elif elements[1] == '__int16':
+            typ = 'uint16_t'
+        elif elements[1] == '__int32':
+            typ = 'uint32_t'
+        elif elements[1] == '__int64':
+            typ = 'uint64_t'
+        elif elements[1] != '':
+            typ = elements[1]
     elif len(elements) == 1:
-        if elements[0] == "__int8":
-            typ = "int8_t"
-        elif elements[0] == "__int16":
-            typ = "int16_t"
-        elif elements[0] == "__int32":
-            typ = "int32_t"
-        elif elements[0] == "__int64":
-            typ = "int64_t"
-        else:
+        if elements[0] == '__int8':
+            typ = 'int8_t'
+        elif elements[0] == '__int16':
+            typ = 'int16_t'
+        elif elements[0] == '__int32':
+            typ = 'int32_t'
+        elif elements[0] == '__int64':
+            typ = 'int64_t'
+        elif elements[0] != '':
             typ = str(tif)
 
     return typ
 
 def get_struct_members(struct, sid):
-    """Get members belonging to a structure by structure ID
     """
+    Get members belonging to a structure by structure ID
+
+    :param struct: Structure object
+    :param sid: Structure ID
+    :return: Dictionary containing structure member information
+    """
+
     members = {}
     for offset, name, size in idautils.StructMembers(sid):
         members[name] = {}
-        members[name]["type"]   = get_member_type(struct, offset)
-        members[name]["offset"] = offset
-        members[name]["size"]   = size
+        typ = get_member_type(struct, offset)
+        if typ:
+            members[name]['type'] = typ
+        else:
+            # Type isn't set so make it a byte array
+            members[name]['type'] = 'uint8_t [{}]'.format(size)
+        members[name]['offset'] = offset
+        members[name]['size']   = size
 
     return members
 
 def get_structs():
-    """Get structures from IDA database
     """
+    Get structures from IDA database
+
+    :return: Dictionary of structures
+    """
+
     structs = {}
     for idx, sid, name in idautils.Structs():
         struct = ida_struct.get_struc(sid)
         structs[name] = {}
-        structs[name]["size"]    = idc.GetStrucSize(sid)
-        structs[name]["members"] = get_struct_members(struct, sid)
+        structs[name]['size']    = ida_struct.get_struc_size(struct)
+        structs[name]['members'] = get_struct_members(struct, sid)
 
     return structs
 
 def main(json_file):
-    """Construct a json file containing analysis data from an IDA database
     """
-    json_array = {}
-    json_array["sections"] = get_sections()
-    json_array["comments"] = get_comments()
-    json_array["names"]  = get_symbols()
-    json_array["structs"]  = get_structs()
+    Construct a json file containing analysis data from an IDA database
 
-    with open(json_file, "wb") as f:
+    :param json_file: Output JSON file name
+    """
+
+    json_array = {}
+    print('[*] Exporting analysis data to {}'.format(json_file))
+    json_array['sections']      = get_sections()
+    json_array['functions']     = get_functions()
+    json_array['func_comments'] = get_function_comments()
+    json_array['line_comments'] = get_line_comments()
+    json_array['names']         = get_names()
+    json_array['structs']       = get_structs()
+    print('[+] Done exporting analysis data')
+
+    with open(json_file, 'w') as f:
         f.write(json.dumps(json_array, indent=4))
 
 if __name__ == '__main__':
-    main(idc.AskFile(1, "*.json", "Export file name"))
+    main(ida_kernwin.ask_file(1, '*.json', 'Export file name'))
